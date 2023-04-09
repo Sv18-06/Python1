d00188ed0b0b930fccf31c33926d50cc10459f74
Svitlana <sv06101981@gmail.com>
2023-04-10 00:57:01 +0300

    Lesson03 was added

---
+++ Lesson3.py
@@ -0,0 +1,10 @@
+fizz, buzz, k = map(int, input().split())
+for i in range(1, k) : 
+    if i%fizz==0 and i%buzz==0:
+        print('FB')
+    elif i%fizz==0:
+        print('Fizz')
+    elif i%buzz==0:
+        print('Buzz')
+    else:
+        print (i)
